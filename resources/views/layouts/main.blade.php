63c494e6fa4884b38d9f6b472c4928d3fb2fb69a
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-06-17 21:03:57 +0200

    Add layouts view

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>TodoApp | @yield('title')</title>
+
+	{{-- Latest compiled and minified CSS --}}
+	<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+	{{-- Font awesome --}}
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+
+@include('layouts.partials.navbar')
+
+<div class="container">
+	@yield('content')
+</div> {{-- /.container --}}
+
+{{-- Jquery --}}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+{{--Latest compiled and minified JavaScript--}}
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/main.js') }}"></script>
+</body>
+</html>
